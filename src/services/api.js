7c833998d35a4cc24f581663da617d08fa870f9f
SouhaylaElAbboudy <souhaylaelabboudy2@gmail.com>
2026-04-23 09:27:48 +0100

    Add Bearer token authentication to API service

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+const token = localStorage.getItem("token");
+
 const API = axios.create({
   baseURL: "http://127.0.0.1:8000/api",
+  headers: {
+    Authorization: `Bearer ${token}`,
+  },
 });
 
 export default API;
